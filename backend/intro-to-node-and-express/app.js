c48d338bd674fe260bd35195a24bc44ba8837948
Fatime Mahamat Sileh <fatimemahamatsileh@gmail.com>
2025-08-13 12:29:07 +0100

    feat: file upload with multer

--- backend/intro-to-node-and-express/app.js
+++ backend/intro-to-node-and-express/app.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 
 const studentRouter = require("./routers/studentRouter");
 const userRouter = require ("./routers/userRouter");
+const upload = require("./middlewares/multer");
 
 const app = express();
 const PORT = process.env.PORT;
@@ -25,6 +26,15 @@ app.use(express.json());
 
 app.use("/students", studentRouter);
 app.use("/auth", userRouter);
+
+app.post("/upload-file", upload.single("avatar"),  (req, res ) => {
+  console.log("file:", req.file);
+
+  res.send({
+    message:"File uploaded."
+  })
+});
+
 
 // Expose the server on the defined port
 app.listen(PORT, () => {
