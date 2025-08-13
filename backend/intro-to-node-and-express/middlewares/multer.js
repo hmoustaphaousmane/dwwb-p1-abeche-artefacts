78a58a8779cb9cce4a2db02f8c00ebdd9d66cf09
Fatime Mahamat Sileh <fatimemahamatsileh@gmail.com>
2025-08-13 15:28:12 +0100

    feat: file upload with multer using memory storage

--- backend/intro-to-node-and-express/middlewares/multer.js
+++ backend/intro-to-node-and-express/middlewares/multer.js
@@ -4,18 +4,20 @@ const { v4 } = require("uuid");
 // const upload = multer({ dest: "public/" });
 
 // Disk Storage
-const storage = multer.diskStorage({
-    destination: function (req, file, next) {
-        next(null, 'public/')
-    },
-    filename: function (req, file, next) {
-        const fileExtention = file.mimetype.split("/")[1];
+// const storage = multer.diskStorage({
+//     destination: function (req, file, next) {
+//         next(null, 'public/')
+//     },
+//     filename: function (req, file, next) {
+//         const fileExtention = file.mimetype.split("/")[1];
 
-        const fileName = v4() + "." + fileExtention;
-        next(null, fileName)
-    }
-})
+//         const fileName = v4() + "." + fileExtention;
+//         next(null, fileName)
+//     }
+// })
 
+// Memory storage
+const storage = multer.memoryStorage()
 
 const upload = multer({ storage });
 
